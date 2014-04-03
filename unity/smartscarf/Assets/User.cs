a6f55fcb5dc035e70609dd1d3ba133c6c4fbfbf1
Necati Munir <NecatiMunir@Necatis-MacBook-Pro.local>
2014-04-03 17:27:15 +0100

    conversion fixed!!

--- unity/smartscarf/Assets/User.cs
+++ unity/smartscarf/Assets/User.cs
@@ -16,23 +16,65 @@ public class User : MonoBehaviour {
 	void Start () {
 		userAudioListener = GetComponent<AudioListener>();
 
-		//micStart();
+		micStart();
 
-		bool yey = false;
+		bool yey = true;
 
 		float[] samples = new float[audio.clip.samples * audio.clip.channels];
 		audio.clip.GetData(samples, 0);
 
+		byte[] ba = ToByteArray(samples);
+
+		float[] samples1 = ToFloatArray (ba);
+
+
 		for (int i = 0; i < samples.Length; ++i)
 		{
-			if (samples[i] != 0f)
+			if (samples[i] != samples1[i])
 			{
-				yey = true;
+				yey = false;
 			}
 		}
-		byte[] ba = new byte[samples.Length * 4];
 
 		print (yey);
+	}
+
+	public byte[] ToByteArray(float[] floatArray) {
+		
+		int len = floatArray.Length * 4;
+		
+		byte[] byteArray = new byte[len];
+		
+		int pos = 0;
+		
+		foreach (float f in floatArray) {
+			
+			byte[] data = System.BitConverter.GetBytes(f);
+			
+			System.Array.Copy(data, 0, byteArray, pos, 4);
+			
+			pos += 4;
+			
+		}
+		
+		return byteArray;
+		
+	}
+
+	public float[] ToFloatArray(byte[] byteArray) {
+		
+		int len = byteArray.Length / 4;
+		
+		float[] floatArray = new float[len];
+		
+		for (int i = 0; i < byteArray.Length; i+=4) {
+			
+			floatArray[i/4] = System.BitConverter.ToSingle(byteArray, i);
+			
+		}
+		
+		return floatArray;
+		
 	}
 	
 	void OnGUI() {
