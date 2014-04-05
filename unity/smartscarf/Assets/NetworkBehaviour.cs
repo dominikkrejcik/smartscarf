0cc2548682f0d9f1a6da924ac3beb5dc7c08c79a
Edward James <Edwards_fun@hotmail.com>
2014-04-05 16:12:37 +0100

    Mic data goes up to the server

---
+++ unity/smartscarf/Assets/NetworkBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+public class NetworkBehaviour: MonoBehaviour {
+	
+	// Use this for initialization
+	
+	
+	const int READ_BUFFER_SIZE = 255;
+	
+	private TcpClient client;
+	private byte[] readBuffer = new byte[READ_BUFFER_SIZE];
+	public string strMessage;
+	private IAsyncResult ar;
+	private Socket m_Socket;
+	void Start () {
+		client = new TcpClient ("54.246.121.80", 8214);
+		
+	}
+	
+	
+	
+	
+	private void read(IAsyncResult ar)
+	{ 
+		int BytesRead;
+		try
+		{
+			// Finish asynchronous read into readBuffer and return number of bytes read.
+			BytesRead = client.GetStream().EndRead(ar);
+			if (BytesRead < 1) 
+			{
+				// if no bytes were read server has close.  
+				
+				return;
+			}
+			// Convert the byte array the message was saved into, minus two for the
+			
+			strMessage = Encoding.ASCII.GetString(readBuffer, 0, BytesRead);
+			
+			print(strMessage);//+"uud");
+			
+			// Start a new asynchronous read into readBuffer.
+			client.GetStream().BeginRead(readBuffer, 0, READ_BUFFER_SIZE, new AsyncCallback(read), null);
+			
+		} 
+		catch
+		{
+			;
+		}
+	}
+	
+	
+	
+	
+	public void SendData(Byte[] data)
+	{
+		BinaryWriter writer = new BinaryWriter(client.GetStream());
+		writer.Write(data);
+		writer.Flush();
+	}
+	
+	
+	
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Start an asynchronous read invoking DoRead to avoid lagging the user
+		// interface.
+		client.GetStream().BeginRead(readBuffer, 0, READ_BUFFER_SIZE, new AsyncCallback(read), null);
+		
+	}
+
+}
+
--- unity/smartscarf/Assets/User.cs
+++ unity/smartscarf/Assets/User.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 public class User : MonoBehaviour {
+	
 	public GameObject soundSource;
 	public AudioClip sound;
+	
 	private bool connected = false;
 	private AudioListener userAudioListener;
+	private NetworkBehaviour networkClass;
 	private List<GameObject> sourceList = new List<GameObject>();
 	private byte[] sampleBytes;
+	
+	
 	// Use thisous for initialization
 	void Start () {
+
 		userAudioListener = GetComponent<AudioListener>();
-		
+		networkClass = GetComponent<NetworkBehaviour>();
+
 		micActivate();
 	}
+
 	public byte[] ToByteArray(float[] floatArray) {
 		
 		int len = floatArray.Length * 4;
@@ -30,11 +39,13 @@ public class User : MonoBehaviour {
 		return byteArray;
 		
 	}
+	
 	public float[] ToFloatArray(byte[] byteArray)
 	{
 		
 		int len = byteArray.Length / 4;
 		float[] floatArray = new float[len];
+		
 		for (int i = 0; i < byteArray.Length; i+=4)
 		{
 			floatArray[i/4] = System.BitConverter.ToSingle(byteArray, i);
@@ -69,14 +80,16 @@ public class User : MonoBehaviour {
 		
 		audio.clip = Microphone.Start(selectedDevice, true, 50, 44100);
 		while (!(Microphone.GetPosition(selectedDevice) > 0)){} // Wait until the recording has started
-		audio.Play(); // Play the audio source!
+		//audio.Play(); // Play the audio source!
 	}
+	
 	byte[] bytesToSend()
 	{
 		float[] audioData = new float[audio.clip.samples * audio.clip.channels];
 		audio.clip.GetData(audioData, 0);
 		
 		return ToByteArray(audioData);
+		
 	}
 	
 	void connect()
@@ -86,58 +99,83 @@ public class User : MonoBehaviour {
 		addSoundSource(sound);
 		addSoundSource(sound);
 		addSoundSource(sound);
+		
 		//wait on success message
 		connected = true;
 		renderer.enabled = true;
 		userAudioListener.enabled = true;
 	}
+	
 	void disconnect()
 	{
 		//break connection from the server
+		
 		removeSoundSources();
+		
 		//wait on success message
 		connected = false;
 		renderer.enabled = false;
 		userAudioListener.enabled = false;
+		
 	}
+
+	IEnumerator sendData ()
+	{
+		yield return new WaitForEndOfFrame();
+
+		networkClass.SendData(bytesToSend());
+	}
+
 	void addSoundSource(AudioClip sound)
 	{
 		Vector3 posistionVector = getNewPosistionVecotr();
+		
 		GameObject source = Instantiate(soundSource, posistionVector, Quaternion.identity) as GameObject;
+		
 		sourceList.Add(source);
+		
 		AudioSource sourceComponent = source.GetComponent<AudioSource>();
 		sourceComponent.PlayOneShot(sound);
 	}
+	
 	Vector3 getNewPosistionVecotr()
 	{
 		//place sources around user
 		
 		return (Quaternion.Euler(0, 0, ((360f)/(sourceList.Count+1)	)) * Vector3.right * 4);
+		
 	}
 	
 	void removeSoundSources()
 	{
+		
 		for (int i=0; i < sourceList.Count; i++)
 		{
 			Destroy(sourceList[i]);
 		}
+		
 		sourceList.Clear();
 	}
+	
 	// Update is called once per frame
 	void Update () {
+		
 		
 		/*
 	 * 
 	 * sending:
 		sampleBytes = bytesToSend();
 		//then send smaplebtess off
+
 		receiving
 		//receive sample bytes
+
 		float[] audioData = ToFloatArray(sampleBytes);
 		audio.clip.SetData(audioData, 0);
+
 	*/
-		byte[] ba = bytesToSend();
 		
-		float[] samples1 = ToFloatArray (ba);
+		StartCoroutine("sendData");
+		
 	}
 }
