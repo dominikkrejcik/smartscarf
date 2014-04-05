2b1ccc05e4ca105b74c41be0b5bf459a018087f5
Edward James <Edwards_fun@hotmail.com>
2014-04-05 15:20:21 +0100

    Fixing bugs

--- unity/smartscarf/Assets/User.cs
+++ unity/smartscarf/Assets/User.cs
@@ -10,67 +10,43 @@ public class User : MonoBehaviour {
 	private bool connected = false;
 	private AudioListener userAudioListener;
 	private List<GameObject> sourceList = new List<GameObject>();
-	private float[] samples;
+	private byte[] sampleBytes;
+
 
 	// Use thisous for initialization
 	void Start () {
+
 		userAudioListener = GetComponent<AudioListener>();
-
-		micStart();
-
-		bool yey = true;
-
-		float[] samples = new float[audio.clip.samples * audio.clip.channels];
-		audio.clip.GetData(samples, 0);
-
-		byte[] ba = ToByteArray(samples);
-
-		float[] samples1 = ToFloatArray (ba);
-
-
-		for (int i = 0; i < samples.Length; ++i)
-		{
-			if (samples[i] != samples1[i])
-			{
-				yey = false;
-			}
-		}
-
-		print (yey);
+		
+		micActivate();
 	}
 
 	public byte[] ToByteArray(float[] floatArray) {
 		
 		int len = floatArray.Length * 4;
-		
 		byte[] byteArray = new byte[len];
-		
 		int pos = 0;
 		
-		foreach (float f in floatArray) {
-			
+		foreach (float f in floatArray)
+		{
 			byte[] data = System.BitConverter.GetBytes(f);
-			
 			System.Array.Copy(data, 0, byteArray, pos, 4);
-			
 			pos += 4;
-			
 		}
 		
 		return byteArray;
 		
 	}
 
-	public float[] ToFloatArray(byte[] byteArray) {
+	public float[] ToFloatArray(byte[] byteArray)
+	{
 		
 		int len = byteArray.Length / 4;
-		
 		float[] floatArray = new float[len];
-		
-		for (int i = 0; i < byteArray.Length; i+=4) {
-			
+
+		for (int i = 0; i < byteArray.Length; i+=4)
+		{
 			floatArray[i/4] = System.BitConverter.ToSingle(byteArray, i);
-			
 		}
 		
 		return floatArray;
@@ -78,7 +54,6 @@ public class User : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-		
 		if (!connected)
 		{
 			if (GUI.Button(new Rect(10, Screen.height-50, Screen.width-20, 30), "Enter room"))
@@ -96,14 +71,23 @@ public class User : MonoBehaviour {
 			}
 		}
 	}
-
-	void micStart()
+	
+	void micActivate()
 	{
 		string selectedDevice = "Built-in Microphone";
-
-		audio.clip = Microphone.Start(selectedDevice, true, 10, 44100);
+		
+		audio.clip = Microphone.Start(selectedDevice, true, 50, 44100);
 		while (!(Microphone.GetPosition(selectedDevice) > 0)){} // Wait until the recording has started
 		audio.Play(); // Play the audio source!
+	}
+
+	byte[] bytesToSend()
+	{
+		float[] audioData = new float[audio.clip.samples * audio.clip.channels];
+		audio.clip.GetData(audioData, 0);
+		
+		return ToByteArray(audioData);
+
 	}
 	
 	void connect()
@@ -167,5 +151,25 @@ public class User : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+
+		/*
+	 * 
+	 * sending:
+		sampleBytes = bytesToSend();
+		//then send smaplebtess off
+
+		receiving
+		//receive sample bytes
+
+		float[] audioData = ToFloatArray(sampleBytes);
+		audio.clip.SetData(audioData, 0);
+
+	*/
+
+		byte[] ba = bytesToSend();
+		
+		float[] samples1 = ToFloatArray (ba);
+
+
 	}
 }
