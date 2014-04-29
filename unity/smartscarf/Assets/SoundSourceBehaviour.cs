dcb6274e06033d61eac082b25ec18e9b7b9229bb
Edward James <Edwards_fun@hotmail.com>
2014-04-29 17:45:40 +0100

    fixed sound managing

--- unity/smartscarf/Assets/SoundSourceBehaviour.cs
+++ unity/smartscarf/Assets/SoundSourceBehaviour.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class SoundSourceBehaviour : MonoBehaviour {
 
 	public GameObject talkIndicator;
+	public Texture2D[] profiles;
+
+	private int ID;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -21,5 +24,16 @@ public class SoundSourceBehaviour : MonoBehaviour {
 		{
 			talkIndicator.renderer.enabled = false;
 		}
+	}
+
+	public int getID()
+	{
+		return ID;
+	}
+
+	public void setID(int id)
+	{
+		ID = id;
+		renderer.material.mainTexture = profiles[id];
 	}
 }
