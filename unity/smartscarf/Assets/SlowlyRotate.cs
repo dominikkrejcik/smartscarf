dfc16c76f785efa5cf9c13045ec099dc9acfed29
Edward James <Edwards_fun@hotmail.com>
2014-04-29 23:57:31 +0100

    Rotate

---
+++ unity/smartscarf/Assets/SlowlyRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowlyRotate : MonoBehaviour {
+
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Time.deltaTime*10, 0, 0);
+		transform.Rotate(0, Time.deltaTime*10, 0, Space.World);
+	}
+}
